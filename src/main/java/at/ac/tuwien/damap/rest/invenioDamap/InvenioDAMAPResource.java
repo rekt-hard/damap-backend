1ecaca06f56ed206c7178a6eaac47782e9c5746a
David Eckhard <dabombeeer@gmail.com>
2022-12-21 10:54:29 +0100

    add draft to add maDMP dataset to DMP

--- src/main/java/at/ac/tuwien/damap/rest/invenioDamap/InvenioDAMAPResource.java
+++ src/main/java/at/ac/tuwien/damap/rest/invenioDamap/InvenioDAMAPResource.java
@@ -1,9 +1,13 @@
 package at.ac.tuwien.damap.rest.invenioDamap;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -13,10 +17,24 @@ import javax.ws.rs.core.MediaType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import at.ac.tuwien.damap.rest.dmp.domain.DmpListItemDO;
+import at.ac.tuwien.damap.domain.Access;
+import at.ac.tuwien.damap.enums.EAccessRight;
+import at.ac.tuwien.damap.enums.EDataAccessType;
+import at.ac.tuwien.damap.enums.EDataKind;
+import at.ac.tuwien.damap.enums.EDataSource;
+import at.ac.tuwien.damap.enums.EDataType;
+import at.ac.tuwien.damap.enums.EIdentifierType;
+import at.ac.tuwien.damap.repo.AccessRepo;
+import at.ac.tuwien.damap.rest.dmp.domain.DatasetDO;
+import at.ac.tuwien.damap.rest.dmp.domain.DmpDO;
+import at.ac.tuwien.damap.rest.dmp.domain.IdentifierDO;
+import at.ac.tuwien.damap.rest.dmp.mapper.MapperService;
 import at.ac.tuwien.damap.rest.dmp.service.DmpService;
+import at.ac.tuwien.damap.rest.madmp.dto.Dataset;
+import at.ac.tuwien.damap.rest.madmp.service.MaDmpService;
 import at.ac.tuwien.damap.security.SecurityService;
 import at.ac.tuwien.damap.validation.AccessValidator;
+import io.quarkus.security.ForbiddenException;
 import io.quarkus.security.UnauthorizedException;
 import lombok.extern.jbosslog.JBossLog;
 
@@ -32,33 +50,32 @@ public class InvenioDAMAPResource {
     AccessValidator accessValidator;
 
     @Inject
+    AccessRepo accessRepo;
+
+    @Inject
     DmpService dmpService;
+
+    @Inject
+    MaDmpService madmpService;
 
     @ConfigProperty(name = "invenio.shared-secret")
     String sharedSecret;
 
-    // ADMIN
-
-    // @GET
-    // @Path("/all")
-    // @RolesAllowed("Damap Admin")
-    // public List<DmpListItemDO> getAll() {
-    // log.info("Return all Dmps");
-    // return dmpService.getAll();
-    // }
-
+    /*
+     * Maybe make it configurable?
+     * Could be more elaborate but should suffice for PoC
+     * Could also go into the {@link at.ac.tuwien.damap.security.SecurityService},
+     * but keeping everything together for PoC
+     * 
+     */
     private boolean validateAuthHeader(HttpHeaders headers) {
-        // Maybe make it configurable?
-        // Could be more elaborate but should suffice for PoC
-        // Could also go into the {@link at.ac.tuwien.damap.security.SecurityService},
-        // but keeping everything together for PoC
         return sharedSecret.equals(headers.getHeaderString("Authorization"));
     }
 
     @GET
     @Path("/person/{personId}")
-    // @RolesAllowed("Damap Admin")
-    public List<DmpListItemDO> getDmpListByPerson(@PathParam String personId, @Context HttpHeaders headers) {
+    public List<DmpDO> getDmpListByPerson(@PathParam String personId,
+            @Context HttpHeaders headers) {
         log.info("Return dmp for person id: " + personId);
 
         // This could allow us to authenticate users with OIDC, and allow admins to
@@ -71,101 +88,105 @@ public class InvenioDAMAPResource {
             }
         }
 
-        return dmpService.getDmpListByPersonId(personId);
+        List<Access> accessList = accessRepo.getAllDmpByUniversityId(personId);
+
+        return accessList.stream().map(access -> {
+            return dmpService.getDmpById(access.getDmp().id);
+        }).collect(Collectors.toList());
     }
 
-    // USER
+    @POST
+    @Path("/{id}/{personId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public DmpDO addDataSetToDMP(@PathParam long id, @PathParam String personId, Dataset dataset,
+            @Context HttpHeaders headers) {
+        log.info("Add dataset to dmp with id: " + id);
 
-    // @GET
-    // @Path("/list")
-    // public List<DmpListItemDO> getDmpList() {
-    // log.info("Return dmp list for user");
-    // String personId = this.getPersonId();
-    // log.info("User id: " + personId);
-    // return dmpService.getDmpListByPersonId(personId);
-    // }
+        if (!(securityService.isAdmin() || securityService.getUserId() == personId)) {
+            if (!validateAuthHeader(headers)) {
+                throw new UnauthorizedException();
+            }
+        }
 
-    /*
-     * @GET
-     * 
-     * @Path("/subordinates")
-     * 
-     * @RolesAllowed("user")
-     * public List<DmpListItemDO> getDmpsSubordinates() {
-     * log.info("Return dmp list for subordinates");
-     * String personId = this.getPersonId();
-     * log.info("User id: " + personId);
-     * // TODO: Service stub
-     * return dmpService.getDmpListByPersonId(personId);
-     * }
-     */
+        if (!accessValidator.canEditDmp(id, personId)) {
+            throw new ForbiddenException("Not authorized to access dmp with id " +
+                    id);
+        }
 
-    // @GET
-    // @Path("/{id}")
-    // public DmpDO getDmpById(@PathParam String id) {
-    // log.info("Return dmp with id: " + id);
-    // String personId = this.getPersonId();
-    // long dmpId = Long.parseLong(id);
-    // if(!accessValidator.canViewDmp(dmpId, personId)){
-    // throw new ForbiddenException("Not authorized to access dmp with id " +
-    // dmpId);
-    // }
-    // return dmpService.getDmpById(dmpId);
-    // }
+        DmpDO dmpDO = dmpService.getDmpById(id);
+        var datasetDO = mapMaDMPDatasetToDatasetDO(dmpDO, new DatasetDO(), dataset, null);
 
-    // @POST
-    // @Consumes(MediaType.APPLICATION_JSON)
-    // public DmpDO saveDmp(@Valid DmpDO dmpDO) {
-    // log.info("Save dmp");
-    // String personId = this.getPersonId();
-    // return dmpService.create(dmpDO, personId);
-    // }
+        dmpDO.getDatasets().add(datasetDO);
+        return dmpService.update(dmpDO);
+    }
 
-    // @PUT
-    // @Path("/{id}")
-    // @Consumes(MediaType.APPLICATION_JSON)
-    // public DmpDO updateDmp(@PathParam String id, @Valid DmpDO dmpDO) {
-    // log.info("Update dmp with id: " + id);
-    // String personId = this.getPersonId();
-    // long dmpId = Long.parseLong(id);
-    // if(!accessValidator.canEditDmp(dmpId, personId)){
-    // throw new ForbiddenException("Not authorized to access dmp with id " +
-    // dmpId);
-    // }
-    // return dmpService.update(dmpDO);
-    // }
+    public DatasetDO mapMaDMPDatasetToDatasetDO(DmpDO dmpDO,
+            DatasetDO datasetDO,
+            Dataset madmpDataset, MapperService mapperService) {
 
-    // @DELETE
-    // @Path("/{id}")
-    // public void deleteDmp(@PathParam String id) {
-    // log.info("Delete dmp with id: " + id);
-    // String personId = this.getPersonId();
-    // long dmpId = Long.parseLong(id);
-    // if (!accessValidator.canDeleteDmp(dmpId, personId)) {
-    // throw new ForbiddenException("Not authorized to delete dmp with id " +
-    // dmpId);
-    // }
-    // dmpService.delete(dmpId);
-    // }
+        // Disclaimer: This is by no means complete. Not all fields of the
+        // Dataset or DMP are set. Null value checks should also be performed.
+        var datasetId = madmpDataset.getDatasetId();
+        if (datasetId != null) {
+            IdentifierDO newId = new IdentifierDO();
+            newId.setIdentifier(datasetId.getIdentifier());
+            newId.setType(EIdentifierType.valueOf(datasetId.getType().name().toUpperCase()));
+            datasetDO.setDatasetId(newId);
+        }
 
-    // private String getPersonId() {
-    // if (securityService == null) {
-    // throw new AuthenticationFailedException("User ID is missing.");
-    // }
-    // return securityService.getUserId();
-    // }
+        datasetDO.setDateOfDeletion(null);
+        datasetDO.setDelete(false);
+        datasetDO.setDeletionPerson(null);
+        datasetDO.setDescription(madmpDataset.getDescription());
+        datasetDO.setLegalRestrictions(null);
+        datasetDO.setLicense("");
+        datasetDO.setSize(0L);
+        // General TODO: some attributes have to be set from distribution
+        if (madmpDataset.getDistribution() != null) {
+            var distributions = madmpDataset.getDistribution();
+            StringBuilder licenseBuilder = new StringBuilder();
+            distributions.forEach(d -> {
+                var dataAccess = EDataAccessType.OPEN;
+                if (d.getDataAccess() != null) {
+                    dataAccess = EDataAccessType.getByValue(d.getDataAccess().name());
+                }
+                datasetDO.setDataAccess(dataAccess);
+                licenseBuilder.append(d.getLicense().stream()
+                        .map(l -> l.getLicenseRef().toString()).collect(Collectors.joining(", ")));
+                datasetDO.setSize(datasetDO.getSize() + d.getByteSize());
+            });
+            datasetDO.setLicense(licenseBuilder.toString());
+        }
 
-    // @GET
-    // @Path("/{id}/{revision}")
-    // public DmpDO getDmpByIdAndRevision(@PathParam String id, @PathParam long
-    // revision) {
-    // log.info("Return dmp with id: " + id + " and revision number: " + revision);
-    // String personId = this.getPersonId();
-    // long dmpId = Long.parseLong(id);
-    // if(!accessValidator.canViewDmp(dmpId, personId)){
-    // throw new ForbiddenException("Not authorized to access dmp with id " +
-    // dmpId);
-    // }
-    // return dmpService.getDmpByIdAndRevision(dmpId, revision);
-    // }
+        datasetDO.setOtherProjectMembersAccess(EAccessRight.READ);
+        datasetDO.setPersonalData(false);
+        datasetDO.setPublicAccess(EAccessRight.READ);
+        datasetDO.setReasonForDeletion("");
+        // TODO: should we set this? If so, what to ?
+        datasetDO.setReferenceHash(null);
+        datasetDO.setRetentionPeriod(null);
+        datasetDO.setSelectedProjectMembersAccess(EAccessRight.READ);
+        datasetDO.setSensitiveData(false);
+        // TODO: Let user decide?
+        datasetDO.setSource(EDataSource.NEW);
+        // This should match the dataset. If new, setDataKind. else setReusedDataKind
+        dmpDO.setDataKind(EDataKind.SPECIFY);
+        dmpDO.setReusedDataKind(EDataKind.SPECIFY);
+        datasetDO.setStartDate(null);
+        datasetDO.setTitle(madmpDataset.getTitle());
+        // Setting data type
+        var types = new ArrayList<EDataType>();
+        var type = EDataType.OTHER;
+        try {
+            log.info("dataset type: " + madmpDataset.getType());
+            type = EDataType.getByValue(madmpDataset.getType());
+        } catch (Exception e) {
+
+        } finally {
+            types.add(type);
+            datasetDO.setType(types);
+        }
+
+        return datasetDO;
+    }
 }
